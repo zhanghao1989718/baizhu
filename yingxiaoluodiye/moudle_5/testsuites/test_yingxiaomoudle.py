f50c87cb8611943a755c257a9ac93d26dd44d55c
zhanghao1989718 <806493156@qq.com>
2019-10-10 15:45:01 +0800

    营销5#moudle

---
+++ yingxiaoluodiye/moudle_5/testsuites/test_yingxiaomoudle.py
@@ -0,0 +1,24 @@
+# _*_ coding:utf-8 _*_
+import unittest
+from selenium import webdriver
+from yingxiaoluodiye.moudle_5.pages.homepage import HomePage
+from yingxiaoluodiye.moudle_5.common.warning import message
+
+hpage = HomePage(webdriver.Chrome())
+class YingXiao_Test(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        hpage.start_browser()
+        message("自动化测试开始")
+
+    @classmethod
+    def tearDownClass(cls):
+        hpage.driver.quit()
+        message("自动化测试结束")
+
+    def test_01(self):
+        hpage.homepage_tuijian()
+
+if __name__ == '__main__':
+    unittest.main(verbosity = 2)
