931060aadf3344739676e75417c519335ad2f6b9
zhanghao1989718 <806493156@qq.com>
2020-02-13 18:06:07 +0800

    爬取厂商返数添加爱奇艺

--- getreturnnum/demo.py
+++ getreturnnum/demo.py
@@ -1,17 +1,22 @@
-a = ['<td>2020-02-07</td>', '<td>2020-02-08</td>', '<td>2020-02-09</td>']
-list = []
-dict = {}
+import json
+import requests
+url = "https://dm.iqiyi.com/pcfirm/clt/actives"
+headers = {
+		   "Content-Type": "application/json;charset=UTF-8",
+		   "Connection": "keep-alive",
+		   "Cookie":"JSESSIONID=35E8CB641DBC9109387153498157BBFD; T00404=93b34a2cde7df4a87bda6d00171e7c0c; __guid=188201405.4112778900262155000.1581492408719; monitor_count=10"
+		   }
+request_param = {
+				 "user_id": "326",
+				 "fromDate": "2020-01-30",
+				 "toDate": "2020-02-05",
+				 "Host":"dm.iqiyi.com",
+				 "User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36"
 
-for b in a:
-    n = 1
-    c = b.split(">")[1].split("<")[0]
-    if n == 1:
-        dict["日期"] = c
-    elif n == 2:
-        dict["出生"] = c
-    else:
-        dict["生日"] = c
-    n += 1
-    list.append(dict)
-print(list)
+				 }
+# response=requests.post(url,data=json.dumps(request_param), headers=headers)
+response = requests.post(url, data=request_param, headers=headers)
+get_text = response.text
+print(get_text)
+
 
